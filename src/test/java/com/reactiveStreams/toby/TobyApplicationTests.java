9cb32afc6b633d1079cebaac96563be745855bf4
02ggang9 <02ggang9@gmail.com>
2023-12-24 14:54:20 +0900

    feat: init

---
+++ src/test/java/com/reactiveStreams/toby/TobyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactiveStreams.toby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
